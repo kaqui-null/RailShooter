4aa02dce651b6560b3d04d8a1600fb1e7092d023
kaqui-null <emanuelgomes24.08@gmail.com>
2026-07-24 16:42:15 -0300

    controle dos caminho dos projeteis

---
+++ scripts/projectile.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var projectileSpeed:float = 600.0
+
+@onready var parent = get_parent()
+@onready var origin = parent.projectileOrigin
+@onready var destination = parent.projectileDestination
+@onready var direction = parent.projectileDirection.normalized()
+
+func _ready():
+	global_position = global_position
+
+
+func _process(delta: float) -> void:
+	global_position += direction * projectileSpeed * delta
+
+	if global_position.distance_to(destination) < projectileSpeed * delta:
+		queue_free()
