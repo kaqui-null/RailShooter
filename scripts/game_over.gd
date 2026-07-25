4dbda18cfb0da76e1124d9eb410bc12a8477341b
kaqui-null <emanuelgomes24.08@gmail.com>
2026-07-25 18:48:45 -0300

    init menu de gameOver

---
+++ scripts/game_over.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var mainMenuScene = preload("res://scenes/main_menu.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_continue_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_try_again_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().change_scene_to_packed(mainMenuScene)
